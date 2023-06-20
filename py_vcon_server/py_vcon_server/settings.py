a69dba822717ea4c18ceb885718121df21350849
dgpetrie <dpetrie@sipez.com>
2023-06-20 15:04:14 -0400

    Made vcon storage config generic to DB implementation

--- py_vcon_server/py_vcon_server/settings.py
+++ py_vcon_server/py_vcon_server/settings.py
@@ -1,8 +1,10 @@
 import os
 from pathlib import Path
 
-REDIS_URL = os.getenv("REDIS_URL", "redis://localhost")
+VCON_STORAGE_TYPE = os.getenv("VCON_STORAGE_TYPE", "redis")
+VCON_STORAGE_URL = os.getenv("STORAGE_URL", "redis://localhost")
 HOSTNAME = os.getenv("HOSTNAME", "http://localhost:8000")
 ENV = os.getenv("ENV", "dev")
 LOG_LEVEL = os.getenv("LOG_LEVEL", "DEBUG")
 LOGGING_CONFIG_FILE = os.getenv("LOGGING_CONFIG_FILE", Path(__file__).parent / 'logging.conf')
+
--- py_vcon_server/tests/test_redis_mgr.py
+++ py_vcon_server/tests/test_redis_mgr.py
@@ -11,9 +11,10 @@ sys.path.append("py_vcon_server/db/redis")
 print("CWD: {}".format(os.getcwd()))
 print(sys.path, file=sys.stderr)
 import py_vcon_server.db.redis.redis_mgr
-from py_vcon_server.settings import REDIS_URL
+from py_vcon_server.settings import VCON_STORAGE_TYPE, VCON_STORAGE_URL
 
-r_mgr = py_vcon_server.db.redis.redis_mgr.RedisMgr(REDIS_URL)
+assert(VCON_STORAGE_TYPE == "redis")
+r_mgr = py_vcon_server.db.redis.redis_mgr.RedisMgr(VCON_STORAGE_URL)
 
 # Run before each test function
 @pytest_asyncio.fixture(autouse=True)
