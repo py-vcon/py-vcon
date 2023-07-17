9aeb6fc0348db8f6b2212176f650f5689b4c5a7b
dgpetrie <dpetrie@sipez.com>
2023-07-17 01:17:55 -0400

    Registration for redis Vcon storage interface

---
+++ py_vcon_server/py_vcon_server/db/redis_registration.py
@@ -0,0 +1,9 @@
+""" Register redis as a VconStorage interface """
+
+import py_vcon_server.db
+import py_vcon_server.db.redis
+import py_vcon_server.db.redis.redis_mgr
+
+# Register the redis implementation of Vcon Storage Interface
+py_vcon_server.db.VconStorage.register("redis", py_vcon_server.db.redis.RedisVconStorage)
+
