5577889098fc92263f8b327c3567b41d50d5b7d1
dgpetrie <dpetrie@sipez.com>
2023-07-23 17:25:20 -0400

    Renamed the fastapi/restful host/port binding parameter, added options to disable admin and vcon rest APIs.

--- py_vcon_server/py_vcon_server/settings.py
+++ py_vcon_server/py_vcon_server/settings.py
@@ -3,8 +3,10 @@ from pathlib import Path
 
 VCON_STORAGE_TYPE = os.getenv("VCON_STORAGE_TYPE", "redis")
 VCON_STORAGE_URL = os.getenv("STORAGE_URL", "redis://localhost")
-HOSTNAME = os.getenv("HOSTNAME", "http://localhost:8000")
+REST_URL = os.getenv("REST_URL", "http://localhost:8000")
 ENV = os.getenv("ENV", "dev")
 LOG_LEVEL = os.getenv("LOG_LEVEL", "DEBUG")
 LOGGING_CONFIG_FILE = os.getenv("LOGGING_CONFIG_FILE", Path(__file__).parent / 'logging.conf')
+LAUNCH_VCON_API = os.getenv("LAUNCH_VCON_API", True)
+LAUNCH_ADMIN_API = os.getenv("LAUNCH_ADMIN_API", True)
 
