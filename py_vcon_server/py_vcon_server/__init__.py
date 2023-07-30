166d6ebfb87be51e3060dbaa22733309421527d5
dgpetrie <dpetrie@sipez.com>
2023-07-30 02:38:43 -0400

    Initialize and set server states

--- py_vcon_server/py_vcon_server/__init__.py
+++ py_vcon_server/py_vcon_server/__init__.py
@@ -9,6 +9,7 @@ sys.path.append("..")
 
 import py_vcon_server.settings
 import py_vcon_server.db
+import py_vcon_server.states
 from py_vcon_server.logging_utils import init_logger
 
 logger = init_logger(__name__)
@@ -21,6 +22,13 @@ for finder, module_name, is_package in pkgutil.iter_modules(py_vcon_server.db.__
   logger.info("db module load: {}".format(module_name))
   importlib.import_module(module_name)
 
+server_state = py_vcon_server.states.ServerState(
+  py_vcon_server.settings.REST_URL,
+  py_vcon_server.settings.STATE_DB_URL,
+  py_vcon_server.settings.LAUNCH_ADMIN_API,
+  py_vcon_server.settings.LAUNCH_VCON_API,
+  {},
+  py_vcon_server.settings.NUM_WORKERS)
 
 import py_vcon_server.vcon_api
 import py_vcon_server.admin_api
@@ -30,13 +38,17 @@ restapi = fastapi.FastAPI()
 @restapi.on_event("startup")
 async def startup():
   logger.info("event startup")
+  await server_state.starting()
   await py_vcon_server.db.VconStorage.setup(py_vcon_server.settings.VCON_STORAGE_TYPE,
     py_vcon_server.settings.VCON_STORAGE_URL)
+  await server_state.running()
 
 @restapi.on_event("shutdown")
 async def shutdown():
   logger.info("event shutdown")
+  await server_state.shutting_down()
   await py_vcon_server.db.VconStorage.teardown()
+  await server_state.unregister()
 
 # Enable Admin entry points
 if(py_vcon_server.settings.LAUNCH_ADMIN_API):
