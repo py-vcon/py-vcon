afe469299163fa996eccad033699e766c3c60775
dgpetrie <dpetrie@sipez.com>
2023-07-30 02:35:40 -0400

    Added state db URL and num_workers settings

--- py_vcon_server/py_vcon_server/settings.py
+++ py_vcon_server/py_vcon_server/settings.py
@@ -3,10 +3,12 @@ from pathlib import Path
 
 VCON_STORAGE_TYPE = os.getenv("VCON_STORAGE_TYPE", "redis")
 VCON_STORAGE_URL = os.getenv("STORAGE_URL", "redis://localhost")
+STATE_DB_URL = os.getenv("STATE_DB_URL", "redis://localhost")
 REST_URL = os.getenv("REST_URL", "http://localhost:8000")
 ENV = os.getenv("ENV", "dev")
 LOG_LEVEL = os.getenv("LOG_LEVEL", "DEBUG")
 LOGGING_CONFIG_FILE = os.getenv("LOGGING_CONFIG_FILE", Path(__file__).parent / 'logging.conf')
 LAUNCH_VCON_API = os.getenv("LAUNCH_VCON_API", True)
 LAUNCH_ADMIN_API = os.getenv("LAUNCH_ADMIN_API", True)
+NUM_WORKERS = os.getenv("NUM_WORKERS", os. cpu_count())
 
