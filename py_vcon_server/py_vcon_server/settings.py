4fda7f78bf757b892169df10684d345160c3256f
dgpetrie <dpetrie@sipez.com>
2023-08-07 18:37:17 -0400

    Added WORK_QUEUES setting and parsing

--- py_vcon_server/py_vcon_server/settings.py
+++ py_vcon_server/py_vcon_server/settings.py
@@ -11,3 +11,24 @@ LAUNCH_VCON_API = os.getenv("LAUNCH_VCON_API", True)
 LAUNCH_ADMIN_API = os.getenv("LAUNCH_ADMIN_API", True)
 NUM_WORKERS = os.getenv("NUM_WORKERS", os. cpu_count())
 
+# parse out optional weights from name for each queue
+WORK_QUEUES = {}
+queue_tokens = os.getenv("WORK_QUEUES", "").split()
+for token in queue_tokens:
+  name_weight = token.split(":")
+  name = name_weight[0]
+  if(len(name_weight) == 1 or
+    name_weight[1] is None or
+    name_weight[1] == ""):
+    weight = 1
+  elif(len(name_weight) == 2):
+    try:
+      weight = int(name_weight[1])
+    except ValueError as e:
+      raise Exception("WORK_QUEUE weights must be an integer value for queue: {}".format(name))
+  else:
+    raise Exception(
+      "Invalid WORK_QUEUE token: {} should be name:weight where name is a string and weight is an integer".
+      format(token))
+  WORK_QUEUES[name] = {"weight": weight}
+
