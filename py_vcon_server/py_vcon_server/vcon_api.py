e1e3ccb680159ec9d28d0c66ad4342f2e939df2e
dgpetrie <dpetrie@sipez.com>
2023-08-07 18:32:31 -0400

    Added more doc to vcon APIs

--- py_vcon_server/py_vcon_server/vcon_api.py
+++ py_vcon_server/py_vcon_server/vcon_api.py
@@ -40,7 +40,9 @@ def init(restapi):
 
   @restapi.post("/vcon")
   async def post_vcon(inbound_vcon: Vcon, vcon_uuid: typing.Union[str, None] = None):
+    """ Store the given vCon in VconStorage, replace if it exists for the given UUID """
     try:
+      logger.debug("setting vcon UUID: {}".format(vcon_uuid))
       vcon = await py_vcon_server.db.VconStorage.set(inbound_vcon.dict())
     except Exception as e:
       logger.info("Error: {}".format(e))
@@ -49,6 +51,7 @@ def init(restapi):
 
   @restapi.delete("/vcon/{vcon_uuid}")
   async def delete_vcon(vcon_uuid: str):
+    """ Delete the vCon idenfied by the given UUID from VconStorage """
     try:
       logger.debug("deleting vcon UUID: {}".format(vcon_uuid))
       await py_vcon_server.db.VconStorage.delete(vcon_uuid)
@@ -63,6 +66,7 @@ def init(restapi):
 
   @restapi.get("/vcon/{vcon_uuid}/jq")
   async def get_vcon_jq_transform(vcon_uuid: str, jq_transform: str):
+    """ Appliy the given jq transfor to the vCon identified by the given UUID and return the results """
     try:
       logger.info("vcon UID: {} jq transform string: {}".format(vcon_uuid, jq_transform))
       transform_result = await py_vcon_server.db.VconStorage.jq_query(vcon_uuid, jq_transform)
@@ -76,6 +80,7 @@ def init(restapi):
 
   @restapi.get("/vcon/{vcon_uuid}/jsonpath")
   async def get_vcon_jsonpath_query(vcon_uuid: str, path_string: str):
+    """ Apply the given JSONpath query to the vCon idntified by the given UUID """
     try:
       logger.info("vcon UID: {} jsonpath query string: {}".format(vcon_uuid, path_string))
       query_result = await py_vcon_server.db.VconStorage.json_path_query(vcon_uuid, path_string)
