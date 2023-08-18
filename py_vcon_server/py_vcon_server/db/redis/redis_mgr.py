ebab1bf9a71edcc5b7fc371559bf5d04b447ebab
dgpetrie <dpetrie@sipez.com>
2023-08-18 03:29:34 -0400

    Cleaned up junk and formating in redis_mgr

--- py_vcon_server/py_vcon_server/db/redis/redis_mgr.py
+++ py_vcon_server/py_vcon_server/db/redis/redis_mgr.py
@@ -8,17 +8,18 @@ redis will be bound to an old loop which will no longer work
 The redis connection pool must be shutdown and restarted when FASTApi does.
 """
 
-import py_vcon_server.logging_utils
 import redis.asyncio.connection
 import redis.asyncio.client
-import pytest_asyncio
-import pytest
-import uuid
+import py_vcon_server.logging_utils
 
 
 logger = py_vcon_server.logging_utils.init_logger(__name__)
 
+class RedisPoolNotInitialized(Exception):
+  """ raised when redis_mgr is not initialized """
+
 class RedisMgr():
+  """ Interface/wrapper for redis clients and the management of them """
 
   def __init__(self, redis_url: str):
     self._redis_url = redis_url
@@ -27,58 +28,59 @@ class RedisMgr():
 
 
   def create_pool(self):
-      self._redis_pool
-      self._redis_pool_initialization_count
-      if self._redis_pool is not None:
-          logger.info("Redis pool already created")
-      else:
-          logger.info("Creating Redis pool...")
-          self._redis_pool_initialization_count += 1
-          options = {"decode_responses": True}
-          self._redis_pool = redis.asyncio.connection.ConnectionPool.from_url(self._redis_url,
-            **options)
-          logger.info(
-              "Redis pool created. redis connection: host: {} port: {} max connections: {} initialization count: {}".format(
-                  self._redis_pool.connection_kwargs.get("host", "None"),
-                  self._redis_pool.connection_kwargs.get("port", "None"),
-                  self._redis_pool.max_connections,
-                  self._redis_pool_initialization_count,
-              )
+    """ Create a redis client pool """
+
+    if self._redis_pool is not None:
+      logger.info("Redis pool already created")
+    else:
+      logger.info("Creating Redis pool...")
+      self._redis_pool_initialization_count += 1
+      options = {"decode_responses": True}
+      self._redis_pool = redis.asyncio.connection.ConnectionPool.from_url(self._redis_url,
+        **options)
+      logger.info(
+        "Redis pool created. redis connection: host: {} port: {} max connections: {} initialization count: {}".format(
+          self._redis_pool.connection_kwargs.get("host", "None"),
+          self._redis_pool.connection_kwargs.get("port", "None"),
+          self._redis_pool.max_connections,
+          self._redis_pool_initialization_count,
           )
-  
-      #logger.debug(dir(self._redis_pool))
-  
-  
+        )
+
+    #logger.debug(dir(self._redis_pool))
+
+
   async def shutdown_pool(self):
-      self._redis_pool
-      if self._redis_pool is not None:
-          logger.info("disconnecting Redis pool")
-          self.log_pool_stats()
-          tmp_pool = self._redis_pool
-          self._redis_pool = None
-          await tmp_pool.disconnect(inuse_connections=True)
-          logger.info("Redis pool shutdown")
-  
-      else:
-          logger.info("Redis pool already disconnected")
-  
-  
+    """ shutdown the client pool and wait for busy ones to complete """
+    if self._redis_pool is not None:
+      logger.info("disconnecting Redis pool")
+      self.log_pool_stats()
+      tmp_pool = self._redis_pool
+      self._redis_pool = None
+      await tmp_pool.disconnect(inuse_connections=True)
+      logger.info("Redis pool shutdown")
+
+    else:
+        logger.info("Redis pool already disconnected")
+
+
   def log_pool_stats(self):
+    """ Log infor about current client pool """
     if(self._redis_pool):
       logger.info("redis pool max: {} in use: {}  available: {}".format(self._redis_pool.max_connections,
         len(self._redis_pool._in_use_connections),
         len(self._redis_pool._available_connections)))
     else:
       logger.info("no active redis pool")
-  
+
   def get_client(self):
+    """ get a redis client from the pool """
     logger.debug("entering get_client")
-    self._redis_pool
     if self._redis_pool is None:
       logger.info("redis_pool is not initialized")
-      raise Exception("redis pool not initialize")
+      raise RedisPoolNotInitialized("redis pool not initialize")
 
-    r = redis.asyncio.client.Redis(connection_pool=self._redis_pool)
-    logger.debug("client type: {}".format(type(r)))
-    return r
+    client = redis.asyncio.client.Redis(connection_pool=self._redis_pool)
+    logger.debug("client type: {}".format(type(client)))
+    return(client)
 
