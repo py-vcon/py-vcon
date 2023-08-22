d091dafbd007422716af06b9e77923f504d2e5fa
dgpetrie <dpetrie@sipez.com>
2023-08-22 19:54:19 -0400

    Added commit method to VconStorage for VconProcessorIO object

--- py_vcon_server/py_vcon_server/db/__init__.py
+++ py_vcon_server/py_vcon_server/db/__init__.py
@@ -3,6 +3,7 @@ import pkgutil
 import importlib
 import vcon
 import py_vcon_server.logging_utils
+import py_vcon_server.processor
 
 logger = py_vcon_server.logging_utils.init_logger(__name__)
 
@@ -61,15 +62,34 @@ class VconStorage():
       raise(Exception("Vcon storage implementation not setup"))
 
     await VconStorage._vcon_storage_binding.set(save_vcon)
-  
+
+  @staticmethod
+  async def commit(processor_output: py_vcon_server.processor.VconProcessorIO) -> None:
+    """
+    Helper function to save changed **Vcon**s from the
+    output of a **VconProcessor** or **Pipeline**.
+
+    Saves **Vcon**s which have been marked as modified
+    or new in the given **VconProcessorIO**
+    """
+    num_vcons = processor_output.num_vcons()
+    for index in range(0, num_vcons):
+      if(processor_output.is_vcon_modified(index)):
+        vcon_dict = await processor_output.get_vcon(
+          index,
+          py_vcon_server.processor.VconTypes.DICT
+          )
+
+        await VconStorage.set(vcon_dict)
+
   @staticmethod
   async def get(vcon_uuid : str) -> typing.Union[None, vcon.Vcon]:
     """ Get a Vcon from storage using its UUID as the key """
     if(VconStorage._vcon_storage_binding is None):
       raise(Exception("Vcon storage implementation not setup"))
 
-    vcon = await VconStorage._vcon_storage_binding.get(vcon_uuid)
-    return(vcon)
+    vCon = await VconStorage._vcon_storage_binding.get(vcon_uuid)
+    return(vCon)
 
   @staticmethod
   async def jq_query(vcon_uuid : str, jq_query_string : str) -> str:
@@ -107,5 +127,4 @@ class VconStorage():
 
 
   # TODO: Need connection status method
-
 
