8cc3be29d5778df54c519172b0f8ecd2c25719fb
dgpetrie <dpetrie@sipez.com>
2023-08-22 19:57:41 -0400

    Added some logging, error checking and better docs for Whisper VconProcessor

--- py_vcon_server/py_vcon_server/processor/builtin/whisper.py
+++ py_vcon_server/py_vcon_server/processor/builtin/whisper.py
@@ -1,9 +1,13 @@
 """ VconProcessor binding for the Vcon Whisper filter_plugin """
 
 import typing
-import asyncio
 import pydantic
 import py_vcon_server.processor
+import py_vcon_server.logging_utils
+
+
+logger = py_vcon_server.logging_utils.init_logger(__name__)
+
 
 __version__ = "0.0.1"
 
@@ -14,7 +18,7 @@ class WhisperInitOptions(py_vcon_server.processor.VconProcessorInitOptions):
     default = "base",
     examples = ["tiny", "base"]
     )
-  
+
 
 class WhisperOptions(py_vcon_server.processor.VconProcessorOptions):
   pass
@@ -28,9 +32,16 @@ class Whisper(py_vcon_server.processor.VconProcessor):
     self,
     init_options: WhisperInitOptions
     ):
+
+    headline = "Whisper OpenAI transcription binding for **VconProcessor**  with model size: " + init_options.model_size
+    description = """
+
+This **VconProcessor** will transcribe one or all of the audio dialogs in the input Vcon and add analysis object(s) containing the transcription for the dialogs.
+The **Whisper** **Vcon** **filter_plug** for transcription is used which is built upon the OpenAI Whisper package.
+      """
     super().__init__(
       "transcribe Vcon dialogs using Vcon Whisper filter_plugin",
-      None,
+      headline + description,
       __version__,
       init_options,
       WhisperOptions,
@@ -43,7 +54,10 @@ class Whisper(py_vcon_server.processor.VconProcessor):
     processor_input: py_vcon_server.processor.VconProcessorIO,
     options: WhisperOptions
     ) -> py_vcon_server.processor.VconProcessorIO:
-
+    """
+    Run the indicated **Vcon** through the **Whisper** **Vcon** **filter_plugin**
+    to transcribe the dialogs.
+    """
     # TODO: check for options on which dialogs to transcribe
     # e.g.:
     # options = {"llanguage" : "en", "model_size" : "base", "output_options" : ["vendor", "word_srt", "word_ass"], "whisper" : { "language" : "en"} }
@@ -53,6 +67,17 @@ class Whisper(py_vcon_server.processor.VconProcessor):
 
     index = options.input_vcon_index
     in_vcon = await processor_input.get_vcon(index)
+    if(in_vcon is None):
+      raise Exception("Vcon not found for index: {}".format(index))
+
+    if(in_vcon.dialog is not None):
+      num_dialog = len(in_vcon.dialog)
+    else:
+      num_dialog = None
+    logger.debug("whisper transcribe on Vcon UUID: {} dialog count: {}".format(
+      in_vcon.uuid,
+      num_dialog
+      ))
 
     out_vcon = in_vcon.whisper(vcon_whisper_options)
 
