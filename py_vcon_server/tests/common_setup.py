8a366b0870dc498d0c9aa0af532a772d3829b348
dgpetrie <dpetrie@sipez.com>
2023-08-22 00:14:58 -0400

    Created fixture to build a Vcon with inline audio dialog

--- py_vcon_server/tests/common_setup.py
+++ py_vcon_server/tests/common_setup.py
@@ -1,4 +1,6 @@
 """ Common test data and tools for conserver unit tests """
+import os 
+import datetime
 import pytest
 import vcon
 
@@ -16,3 +18,23 @@ def make_2_party_tel_vcon() -> vcon.Vcon:
 
   return(vCon)
 
+
+@pytest.fixture(scope="function")
+def make_inline_audio_vcon(make_2_party_tel_vcon: vcon.Vcon) -> vcon.Vcon:
+  vCon = make_2_party_tel_vcon
+
+  file_path = "tests/hello.wav"
+  file_content = b""
+  with open(file_path, "rb") as file_handle:
+    file_content = file_handle.read()
+    print("body length: {}".format(len(file_content)))
+    assert(len(file_content) > 10000)
+
+    vCon.add_dialog_inline_recording(file_content,
+    datetime.datetime.utcnow(),
+    0, # duration TODO
+    [0,1],
+    vcon.Vcon.MIMETYPE_AUDIO_WAV,
+    os.path.basename(file_path))
+
+  return(vCon)
