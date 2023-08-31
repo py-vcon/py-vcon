f9b73a429815e86a7bfd92c74ba8437a3f5bd36e
dgpetrie <dpetrie@sipez.com>
2023-08-31 17:13:21 -0400

    Registration for OpenAI completion filter_plugin

---
+++ vcon/filter_plugins/openai.py
@@ -0,0 +1,15 @@
+""" OpenAi filter plugin registrations """
+import os
+import vcon.filter_plugins
+
+openai_api_key = os.getenv("OPENAI_API_KEY", None)
+init_options = {"openai_api_key": openai_api_key}
+
+vcon.filter_plugins.FilterPluginRegistry.register(
+  "openai_completion",
+  "vcon.filter_plugins.impl.openai",
+  "OpenAICompletion",
+  "OpenAI completion generative AI",
+  init_options
+  )
+
