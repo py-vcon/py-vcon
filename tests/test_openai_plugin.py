77dbb97e00ffc986721f4759a64038d00b62f03c
dgpetrie <dpetrie@sipez.com>
2023-09-21 22:41:19 -0400

    Added openai plugin unit test for case when transcript needs to be generated.

--- tests/test_openai_plugin.py
+++ tests/test_openai_plugin.py
@@ -8,6 +8,7 @@ import vcon.filter_plugins.impl.openai
 
 TEST_EXTERNAL_AUDIO_VCON_FILE = "tests/example_external_dialog.vcon"
 TEST_DIARIZED_EXTERNAL_AUDIO_VCON_FILE = "tests/example_deepgram_external_dialog.vcon"
+SHORT_RECORDING_VCON = "tests/hello.vcon"
 
 def test_1_options():
   """ Tests for OpenAICompletionInitOptions and OpenAICompletionOptions """
@@ -199,5 +200,57 @@ def test_4_diarized_chat_completion_object_summary():
   assert(len(out_vcon.analysis[original_analysis_count]["body"]["choices"][0]["message"]["content"]) > 250)
   assert(out_vcon.analysis[original_analysis_count]["model"] == "gpt-4")
   print("Response: " + out_vcon.analysis[original_analysis_count]["body"]["choices"][0]["message"]["content"])
+
+
+def test_5_openai_triggers_trasncribe():
+  """ Test OpenAIChatCompletion FilterPlugin with missing transcribe ananlysis, should transcribe"""
+
+  in_vcon = vcon.Vcon()
+  in_vcon.load(SHORT_RECORDING_VCON)
+  # Remove transcription or any other analysis
+  in_vcon.analysis.clear()
+  assert(len(in_vcon.analysis) == 0)
+  original_analysis_count = 0
+
+  options = {"jq_result": "."}
+
+  out_vcon = None
+
+  try:
+    out_vcon = in_vcon.openai_chat_completion(options)
+
+  except pydantic.error_wrappers.ValidationError as e:
+    openai_key = os.getenv("OPENAI_API_KEY", None)
+    if(openai_key is None or
+      openai_key == ""):
+        raise Exception("OPENAI_API_KEY environment variable not set this test cannot run") from e
+    raise e
+
+  after_analysis_count = len(out_vcon.analysis)
+  assert(after_analysis_count == 4) # transcription and openai summary
+
+  # Check stats on input to model:
+  plugin = vcon.filter_plugins.FilterPluginRegistry.get("openai_chat_completion").plugin()
+  # verify stats of extracted messages are as expected
+  print("stats: {}".format(plugin.last_stats))
+  assert(plugin.last_stats['num_messages'] == 1)
+  assert(plugin.last_stats['num_text_dialogs'] == 0)
+  assert(plugin.last_stats['num_dialog_list'] == 1)
+  assert(plugin.last_stats['num_transcribe_analysis'] == 1)
+
+  assert((after_analysis_count - original_analysis_count) == 4)
+  assert(out_vcon.analysis[original_analysis_count + 3]["type"] == "summary")
+  assert(out_vcon.analysis[original_analysis_count + 3]["dialog"] == 0)
+  assert(out_vcon.analysis[original_analysis_count + 3]["vendor"] == "openai")
+  assert(out_vcon.analysis[original_analysis_count + 3]["product"] == "ChatCompletion")
+  assert(out_vcon.analysis[original_analysis_count + 3]["schema"] == "chat_completion_object")
+  assert(out_vcon.analysis[original_analysis_count + 3]["prompt"] == "Summarize the transcript in these messages.")
+  assert(out_vcon.analysis[original_analysis_count + 3]["mimetype"] == vcon.Vcon.MIMETYPE_JSON)
+  assert(out_vcon.analysis[original_analysis_count + 3]["encoding"] == "json")
+  assert(isinstance(out_vcon.analysis[original_analysis_count + 3]["body"], dict))
+  assert(isinstance(out_vcon.analysis[original_analysis_count + 3]["body"]["choices"][0]["message"]["content"], str))
+  assert(len(out_vcon.analysis[original_analysis_count + 3]["body"]["choices"][0]["message"]["content"]) > 80)
+  assert(out_vcon.analysis[original_analysis_count + 3]["model"] == "gpt-4")
+  print("Response: " + out_vcon.analysis[original_analysis_count + 3]["body"]["choices"][0]["message"]["content"])
 
 
