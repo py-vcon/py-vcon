bd1626c9e9cdc9581bd2604ffbae7eccbb896baf
dgpetrie <dpetrie@sipez.com>
2023-09-26 16:01:17 -0400

    removed old server

--- server/lib/error_tracking.py
+++
@@ -1,7 +0,0 @@
-import os
-from lib.error_trackers.sentry import init_sentry
-
-
-def init_error_tracker():
-    if os.environ.get("SENTRY_DSN"):
-        init_sentry()
--- server/lib/logging_utils.py
+++
@@ -1,9 +0,0 @@
-import logging
-import logging.config
-from settings import LOGGING_CONFIG_FILE
-
-logging.config.fileConfig(LOGGING_CONFIG_FILE)
-
-
-def init_logger(name):
-    return logging.getLogger(name)
