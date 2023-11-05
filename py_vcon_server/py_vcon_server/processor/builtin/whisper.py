0ae13bafbec0b0d3a2231882560c1f91a0b7700a
dgpetrie <dpetrie@sipez.com>
2023-11-05 08:03:04 -0500

    Converted Whisper Processor to use FilterPluginProcessor

--- py_vcon_server/py_vcon_server/processor/builtin/whisper.py
+++ py_vcon_server/py_vcon_server/processor/builtin/whisper.py
@@ -9,91 +9,31 @@ import vcon.filter_plugins
 logger = py_vcon_server.logging_utils.init_logger(__name__)
 
 
-__version__ = "0.0.1"
-
-plugin = vcon.filter_plugins.FilterPluginRegistry.get("whisper")
-
-
-class WhisperInitOptions(
-  py_vcon_server.processor.VconProcessorInitOptions,
-  plugin.plugin().init_options_type
-  ):
-  pass
-  # The folloiwng should be defined in the WhisperInitOptions
-  # model_size: typing.Optional[str] = pydantic.Field(
-  #   title = "Whisper model size to use",
-  #   #description = "vCon format version,
-  #   default = "base",
-  #   examples = ["tiny", "base"]
-  #   )
+PLUGIN_NAME = "whisper"
+CLASS_NAME = "Whisper"
+PLUGIN = vcon.filter_plugins.FilterPluginRegistry.get(PLUGIN_NAME)
 
 
-class WhisperOptions(
-  py_vcon_server.processor.VconProcessorOptions,
-  plugin.plugin().options_type
-  ):
-  pass
-  # TODO: add options for specifying which dialogs to transcribe
+WhisperInitOptions = py_vcon_server.processor.FilterPluginProcessor.makeInitOptions(CLASS_NAME, PLUGIN)
 
-# TODO: REMOVE THIS
-print("WhisperOptions fields: {}".format(WhisperOptions.__fields__))
 
-class Whisper(py_vcon_server.processor.VconProcessor):
+WhisperOptions = py_vcon_server.processor.FilterPluginProcessor.makeOptions(CLASS_NAME, PLUGIN)
+
+
+class Whisper(py_vcon_server.processor.FilterPluginProcessor):
   """ Whisper OpenAI transcription binding for **VconProcessor** """
-  def __init__(
-    self,
-    init_options: WhisperInitOptions
-    ):
-
-    headline = "Whisper OpenAI transcription binding for **VconProcessor**  with model size: " + init_options.model_size
-    description = """
+  plugin_version = "0.0.1"
+  plugin_name = "whisper"
+  options_class =  WhisperOptions
+  headline = "Whisper OpenAI transcription binding for **VconProcessor**  with model size: {}".format(
+    PLUGIN.plugin().init_options().model_size)
+  # TODO: add the following to description: 
+  plugin_description = """
 
 This **VconProcessor** will transcribe one or all of the audio dialogs in the input Vcon and add analysis object(s) containing the transcription for the dialogs.
 The **Whisper** **Vcon** **filter_plug** for transcription is used which is built upon the OpenAI Whisper package.
       """
-    super().__init__(
-      "transcribe Vcon dialogs using Vcon Whisper filter_plugin",
-      headline + description,
-      __version__,
-      init_options,
-      WhisperOptions,
-      True # modifies a Vcon
-      )
+
 
     #TODO: register different Vcon filter_plugin for each model size
-
-  async def process(self,
-    processor_input: py_vcon_server.processor.VconProcessorIO,
-    options: WhisperOptions
-    ) -> py_vcon_server.processor.VconProcessorIO:
-    """
-    Run the indicated **Vcon** through the **Whisper** **Vcon** **filter_plugin**
-    to transcribe the dialogs.
-    """
-    # TODO: check for options on which dialogs to transcribe
-    # e.g.:
-    # options = {"llanguage" : "en", "model_size" : "base", "output_options" : ["vendor", "word_srt", "word_ass"], "whisper" : { "language" : "en"} }
-    #  if(options.tel is not None and
-    #    options.tel != ""):
-    vcon_whisper_options = {}
-
-    index = options.input_vcon_index
-    in_vcon = await processor_input.get_vcon(index)
-    if(in_vcon is None):
-      raise Exception("Vcon not found for index: {}".format(index))
-
-    if(in_vcon.dialog is not None):
-      num_dialog = len(in_vcon.dialog)
-    else:
-      num_dialog = None
-    logger.debug("whisper transcribe on Vcon UUID: {} dialog count: {}".format(
-      in_vcon.uuid,
-      num_dialog
-      ))
-
-    out_vcon = await in_vcon.whisper(vcon_whisper_options)
-
-    await processor_input.update_vcon(out_vcon)
-
-    return(processor_input)
 
