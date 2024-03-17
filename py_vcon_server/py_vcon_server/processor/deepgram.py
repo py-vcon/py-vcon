bc3e5059cf9f8b7174655f1e7bfd6e131227403f
dgpetrie <dpetrie@sipez.com>
2024-03-17 01:18:26 -0400

    Use correct init_options type for Deepgram processor

--- py_vcon_server/py_vcon_server/processor/deepgram.py
+++ py_vcon_server/py_vcon_server/processor/deepgram.py
@@ -4,7 +4,7 @@ import py_vcon_server.processor
 import py_vcon_server.processor.builtin.deepgram
 
 deepgram_key = os.getenv("DEEPGRAM_KEY", "")
-init_options = py_vcon_server.processor.VconProcessorInitOptions(deepgram_key = deepgram_key)
+init_options = py_vcon_server.processor.builtin.deepgram.DeepgramInitOptions(deepgram_key = deepgram_key)
 
 py_vcon_server.processor.VconProcessorRegistry.register(
       init_options,
