469d7cb8ac8d7c3edc9ee555187faa3ff965cf5a
dgpetrie <dpetrie@sipez.com>
2024-04-26 17:06:09 -0400

    Add test registration for use with testing PLUGIN_PATHS

---
+++ py_vcon_server/tests/processors/test_add_party.py
@@ -0,0 +1,13 @@
+# Copyright (C) 2023-2024 SIPez LLC.  All rights reserved.
+""" Registration for the test AddParty **VconProcessor** """
+import py_vcon_server.processor
+
+init_options = py_vcon_server.processor.VconProcessorInitOptions()
+
+py_vcon_server.processor.VconProcessorRegistry.register(
+      init_options,
+      "test_add_party",
+      "processors_good",
+      "AddParty"
+      )
+
