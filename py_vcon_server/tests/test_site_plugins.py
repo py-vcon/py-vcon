79837e95d8999e9486f5e30fff98966068acf8fd
dgpetrie <dpetrie@sipez.com>
2024-04-29 16:20:41 -0400

    Modified the site process dir in the site processor unit tests and added additional test points.

--- py_vcon_server/tests/test_site_plugins.py
+++ py_vcon_server/tests/test_site_plugins.py
@@ -4,10 +4,12 @@ import os
 import importlib
 import py_vcon_server
 
-os.environ["PLUGIN_PATHS"] = "tests/processors,ddd"
+ORIGINAL_PLUGIN_PATHS = os.getenv("PLUGIN_PATHS", None)
 
 def test_plugin_path():
   try:
+    assert("test_processors" not in os.getenv("PLUGIN_PATHS", "").split(","))
+    assert("test_processors" not in py_vcon_server.settings.PLUGIN_PATHS)
     proc = py_vcon_server.processor.VconProcessorRegistry.get_processor_instance("test_add_party")
     assert(proc is None)
   except py_vcon_server.processor.VconProcessorNotRegistered as not_found:
@@ -15,8 +17,14 @@ def test_plugin_path():
     pass
 
   # Force a reload as we have changed the PLUGIN_PATHS env var
+  os.environ["PLUGIN_PATHS"] = "test_processors,ddd"
   importlib.reload(py_vcon_server.settings)
   importlib.reload(py_vcon_server)
+  assert("test_processors" in py_vcon_server.settings.PLUGIN_PATHS)
+  if(ORIGINAL_PLUGIN_PATHS is None):
+    del os.environ["PLUGIN_PATHS"]
+  else:
+    os.environ["PLUGIN_PATHS"] = ORIGINAL_PLUGIN_PATHS
 
   proc = py_vcon_server.processor.VconProcessorRegistry.get_processor_instance("test_add_party")
   assert(proc is not None)
