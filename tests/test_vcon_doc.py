6bd82aace3a0e5dbf191a112991d8793582b40be
dgpetrie <dpetrie@sipez.com>
2024-07-21 21:37:33 -0400

    Updated expected untagged method count for Vcon doc

--- tests/test_vcon_doc.py
+++ tests/test_vcon_doc.py
@@ -97,7 +97,7 @@ def test_vcon_doc():
   print("got {} tagged of {} methods".format(num_tagged_methods, total_methods))
   print("untagged methods: {}".format(untagged_methods))
   print("tags: {}".format(list(tagged_methods.keys())))
-  assert(total_methods - num_tagged_methods <= 12)
+  assert(total_methods - num_tagged_methods <= 14)
   # if the tags change, may need to check the layout and order
   assert(len(tagged_methods) == 9)
 
