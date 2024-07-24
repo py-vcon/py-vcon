ce1d7dabff9aa742bea0a15d3699d12f86fb9098
dgpetrie <dpetrie@sipez.com>
2024-07-23 20:44:17 -0400

    Unit test for redaction object

---
+++ tests/test_redacted.py
@@ -0,0 +1,11 @@
+
+
+import vcon
+
+def test_empty_vcon():
+  empty_vcon = vcon.Vcon()
+  empty_vcon.set_uuid("py-vcon.dev")
+  empty_vcon.set_redacted("1234", "PII")
+  assert(empty_vcon.redacted["uuid"] == "1234")
+  assert(empty_vcon.redacted["type"] == "PII")
+
