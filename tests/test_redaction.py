4e2a96038edd80b411ece64bd331cccd187615e2
dgpetrie <dpetrie@sipez.com>
2024-10-11 18:26:18 -0400

    Changed names in redaction unit test stay in sink with filter_plugin

--- tests/test_redaction.py
+++ tests/test_redaction.py
@@ -1,30 +1,29 @@
+# Copyright (C) 2023-2024 SIPez LLC.  All rights reserved.
 """ Unit test for the sample redaction filter plugin """
 
-import sys
-import vcon
-import pytest
 import json
-import asyncio
+import pytest
+import vcon
 # Register and load the redaction filter plugin
 import examples.redact_vcon
- 
+
 TRANSCRIBED_VCON_FILE       = "tests/example_deepgram_external_dialog.vcon"
 VCON_WITHOUT_TRANSCRIPTION  = "examples/test.vcon"
 
 pytest_plugins = ('pytest_asyncio')
 
 @pytest.mark.asyncio
-async def test_redaction():
-  redaction_plugin = vcon.filter_plugins.FilterPluginRegistry.get("redact")
-  init_options = examples.redact_vcon.RedactInitOptions()
-  options = examples.redact_vcon.RedactOptions()
+async def test_pii_redaction():
+  redaction_plugin = vcon.filter_plugins.FilterPluginRegistry.get("redact_pii")
+  init_options = examples.redact_vcon.RedactPiiInitOptions()
+  options = examples.redact_vcon.RedactPiiOptions()
   assert(redaction_plugin is not None)
   assert(redaction_plugin.import_plugin(init_options))
-  
+
   # VCon with transcription should have redacted dialog
   input_transcribed_vcon = vcon.Vcon()
   input_transcribed_vcon.load(TRANSCRIBED_VCON_FILE)
-  out_redacted_vcon = await input_transcribed_vcon.redact(options)
+  out_redacted_vcon = await input_transcribed_vcon.redact_pii(options)
   out_redacted_json = json.dumps(out_redacted_vcon.dumps(), indent=4)
   assert(examples.redact_vcon.ANALYSIS_TYPE in out_redacted_json)
 
@@ -35,11 +34,11 @@ async def test_redaction():
   #out_json = json.dumps(out_vcon.dumps(), indent=4)
   #assert(examples.redact_vcon.ANALYSIS_TYPE not in out_json)
 
-  print(out_redacted_json)
+  out_redacted_vcon.dump("pii_redacted.vcon")
 
   # Save the redacted output
   #with open("tests/redacted_vcon.json", "w") as output_file:
   #  output_file.write(out_redacted_json)
-    
+
   vcon.filter_plugins.FilterPluginRegistry.shutdown_plugins()
 
