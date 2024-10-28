b6deb7bac300686cb0b084275b161a0c17310268
dgpetrie <dpetrie@sipez.com>
2024-10-28 19:57:27 -0400

    More slop on deepgram transcript size

--- tests/test_deepgram.py
+++ tests/test_deepgram.py
@@ -61,7 +61,7 @@ async def test_deepgram_transcribe_inline_dialog():
 
   text_list = await out_vcon.get_dialog_text(0)
   print("text: {}".format(json.dumps(text_list, indent = 2)))
-  assert(52 <= len(text_list) <= 65)
+  assert(52 <= len(text_list) <= 75)
 
   # Run again, should not generate duplicate analysis
   out_vcon2 = await out_vcon.deepgram({})
