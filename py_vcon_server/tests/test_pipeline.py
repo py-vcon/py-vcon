afd96bb440ba21b9c7c2889a69c97c443935a2ee
dgpetrie <dpetrie@sipez.com>
2024-11-02 15:33:39 -0400

    Don't dump pydantic model parameters with None value in pipeline tests

--- py_vcon_server/tests/test_pipeline.py
+++ py_vcon_server/tests/test_pipeline.py
@@ -435,10 +435,10 @@ async def test_pipeline_restapi(make_inline_audio_vcon: vcon.Vcon):
     assert(len(pipe_def.processors) == 2)
     assert(pipe_def.processors[0].processor_name == "deepgram")
     # Expecting: format_options and input_vcon_index in dict
-    assert(len(pipe_def.processors[0].processor_options.dict()) == 2)
+    assert(len(pipe_def.processors[0].processor_options.dict(exclude_none=True)) == 2)
     assert(pipe_def.processors[0].processor_options.input_vcon_index == 0)
     assert(pipe_def.processors[1].processor_name == "openai_chat_completion")
-    assert(len(pipe_def.processors[1].processor_options.dict()) == 2)
+    assert(len(pipe_def.processors[1].processor_options.dict(exclude_none=True)) == 2)
     assert(pipe_def.processors[1].processor_options.input_vcon_index == 0)
 
 
