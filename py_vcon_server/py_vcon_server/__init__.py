215922563bd0a59b2b79f31a2ce2a9cc6ad2b15f
dgpetrie <dpetrie@sipez.com>
2025-06-12 15:46:05 -0400

    Bumped server to version 0.4.0

--- py_vcon_server/py_vcon_server/__init__.py
+++ py_vcon_server/py_vcon_server/__init__.py
@@ -23,7 +23,7 @@ logger.debug("root logging handlers: {}".format(logging.getLogger().handlers))
 logger.debug("logging handlers: {}".format(logger.handlers))
 nest_asyncio.apply()
 
-__version__ = "0.3.2"
+__version__ = "0.4.0"
 
 JOB_INTERFACE = None
 JOB_MANAGER = None
