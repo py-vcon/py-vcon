0e13eed724add1ecf95e50bc490f57641fc95c30
dgpetrie <dpetrie@sipez.com>
2025-10-26 01:31:58 -0400

    Updated copyrigth date for jq_redaction unit test and registration

--- vcon/filter_plugins/jq_redaction.py
+++ vcon/filter_plugins/jq_redaction.py
@@ -1,4 +1,4 @@
-# Copyright (C) 2023-2024 SIPez LLC.  All rights reserved.
+# Copyright (C) 2023-2025 SIPez LLC.  All rights reserved.
 """ JQ defined vCon Redaction plugin registration """
 import typing
 import vcon.filter_plugins
