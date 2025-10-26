3b2842bfc9cc0aaa12971f22bb09840c3fc46c05
dgpetrie <dpetrie@sipez.com>
2025-10-26 01:34:39 -0400

    Bump vcon versaion to 0.6.1

--- vcon/__init__.py
+++ vcon/__init__.py
@@ -33,7 +33,7 @@ import vcon.security
 import vcon.filter_plugins
 import vcon.accessors
 
-__version__ = "0.6.0"
+__version__ = "0.6.1"
 
 def build_logger(name : str) -> logging.Logger:
   logger = logging.getLogger(name)
