ff4751fd71202f58093781afe1c43e7265bbd645
dgpetrie <dpetrie@sipez.com>
2025-11-11 19:54:01 -0500

    Fixed transcript accessor for Deepgram to work with multichannel recordings

--- vcon/filter_plugins/deepgram.py
+++ vcon/filter_plugins/deepgram.py
@@ -1,4 +1,4 @@
-# Copyright (C) 2023-2024 SIPez LLC.  All rights reserved.
+# Copyright (C) 2023-2025 SIPez LLC.  All rights reserved.
 """ Deepgram audio transcription filter plugin registration """
 import os
 import datetime
@@ -35,27 +35,41 @@ class DeepgramTranscriptAccessor(vcon.accessors.TranscriptAccessor):
 
       text_list = []
       dialog_start = datetime.datetime.fromisoformat(vcon.utils.cannonize_date(self._dialog_dict["start"]))
-      # get diarization if provided
-      if(self._analysis_dict["body"]["results"]["channels"][0]["alternatives"][0].get("paragraphs", None) is not None):
-        for paragraph in self._analysis_dict["body"]["results"]["channels"][0]["alternatives"][0]["paragraphs"]["paragraphs"]:
-          text_dict = {}
-          text_dict["parties"] = paragraph["speaker"]
-          relative_start = paragraph["start"]
-          text_dict["start"] = (dialog_start + datetime.timedelta(0, relative_start)).isoformat()
-          relative_end = paragraph["end"]
-          text_dict["duration"] = relative_end - relative_start
-          sentence_list = [d["text"] for d in paragraph["sentences"]]
-          text_dict["text"] = "  ".join(sentence_list)
-          text_list.append(text_dict)
 
-      else:
-        text_dict["parties"] = self._dialog_dict["parties"]
-        text_dict["text"] = self._analysis_dict["body"]["results"]["channels"][0]["alteratives"]["transcript"]
-        relative_start = self._analysis_dict["body"]["channels"][0]["alteratives"]["words"][0]["start"]
-        text_dict["start"] = (dialog_start + datetime.timedelta(0, relative_start)).isoformat()
-        relative_end = self._analysis_dict["body"]["channels"][0]["alternatives"]["words"][-1]["end"]
-        text_dict["duration"] = relative_end - relative_start
-        text_list.append(text_dict)
+      num_channels = len(self._analysis_dict["body"]["results"]["channels"])
+      for channel_index in range(num_channels):
+
+        # get diarization if provided
+        if(self._analysis_dict["body"]["results"]["channels"][channel_index]["alternatives"][0].get("paragraphs", None) is not None):
+          channel_parties = []
+          # Need to get the number of parties for each channel so that we can
+          # use a unique index for each.
+
+          for paragraph in self._analysis_dict["body"]["results"]["channels"][channel_index]["alternatives"][0]["paragraphs"]["paragraphs"]:
+
+            for sentence in paragraph["sentences"]:
+              text_dict = {}
+              if(num_channels == 1):
+                text_dict["parties"] = paragraph["speaker"]
+              else:
+                # use the channel index if multichannel
+                text_dict["parties"] = channel_index
+              relative_start = sentence["start"]
+              text_dict["start"] = (dialog_start + datetime.timedelta(0, relative_start)).isoformat()
+              relative_end = sentence["end"]
+              text_dict["duration"] = relative_end - relative_start
+              #sentence_list = [d["text"] for d in paragraph["sentences"]]
+              text_dict["text"] = sentence["text"]
+              text_list.append(text_dict)
+
+        else:
+          text_dict["parties"] = self._dialog_dict["parties"]
+          text_dict["text"] = self._analysis_dict["body"]["results"]["channels"][channel_index]["alteratives"]["transcript"]
+          relative_start = self._analysis_dict["body"]["channels"][channel_index]["alteratives"]["words"][0]["start"]
+          text_dict["start"] = (dialog_start + datetime.timedelta(0, relative_start)).isoformat()
+          relative_end = self._analysis_dict["body"]["channels"][channel_index]["alternatives"]["words"][-1]["end"]
+          text_dict["duration"] = relative_end - relative_start
+          text_list.append(text_dict)
 
       return(text_list)
 
