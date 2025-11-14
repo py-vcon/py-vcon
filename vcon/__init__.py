da7dfc6c32fc7cdc2f77db218f3fc67709e0bef8
dgpetrie <dpetrie@sipez.com>
2025-11-14 15:34:02 -0500

    Bumped the python-vcon version to 0.6.3

--- vcon/__init__.py
+++ vcon/__init__.py
@@ -33,7 +33,7 @@ import vcon.security
 import vcon.filter_plugins
 import vcon.accessors
 
-__version__ = "0.6.2"
+__version__ = "0.6.3"
 
 def build_logger(name : str) -> logging.Logger:
   logger = logging.getLogger(name)
