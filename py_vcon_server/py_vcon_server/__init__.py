82fbcb43df3d3f40daefe57b6384b4f21d223a64
dgpetrie <dpetrie@sipez.com>
2026-03-17 15:49:18 -0400

    Bumped py_vcon_server to 0.5.7

--- py_vcon_server/py_vcon_server/__init__.py
+++ py_vcon_server/py_vcon_server/__init__.py
@@ -1,4 +1,4 @@
-# Copyright (C) 2023-2025 SIPez LLC.  All rights reserved.
+# Copyright (C) 2023-2026 SIPez LLC.  All rights reserved.
 import sys
 import time
 import asyncio
@@ -23,7 +23,7 @@ logger.debug("root logging handlers: {}".format(logging.getLogger().handlers))
 logger.debug("logging handlers: {}".format(logger.handlers))
 nest_asyncio.apply()
 
-__version__ = "0.5.4"
+__version__ = "0.5.7"
 
 JOB_INTERFACE = None
 JOB_MANAGER = None
