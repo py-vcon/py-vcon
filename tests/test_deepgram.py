bb81ca6004467adac40a391da753faa01b02458d
dgpetrie <dpetrie@sipez.com>
2026-05-28 19:46:17 -0400

    Some Deepgram models are producing few text segments.  Loosen the constraints.

--- tests/test_deepgram.py
+++ tests/test_deepgram.py
@@ -62,7 +62,7 @@ async def test_deepgram_transcribe_inline_dialog():
 
   text_list = await out_vcon.get_dialog_text(0)
   print("text: {}".format(json.dumps(text_list, indent = 2)))
-  assert(100 <= len(text_list) <= 170)
+  assert(30 <= len(text_list) <= 170)
 
   # Run again, should not generate duplicate analysis
   out_vcon2 = await out_vcon.deepgram({})
