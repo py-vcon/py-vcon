abd8a3164d862512ba5d916ff353216afcaa0bb7
dgpetrie <dpetrie@sipez.com>
2026-05-30 13:39:52 -0400

    Bumped server version to 0.5.20  Please enter the commit message for your changes. Lines starting

--- py_vcon_server/py_vcon_server/__init__.py
+++ py_vcon_server/py_vcon_server/__init__.py
@@ -43,7 +43,7 @@ try:
 except ValueError:
   pass  # uvloop already patched this loop; nest_asyncio not needed
 
-__version__ = "0.5.19"
+__version__ = "0.5.20"
 
 JOB_INTERFACE = None
 JOB_MANAGER = None
